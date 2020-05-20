f4565fe1977e182d7e4999c3ea151a3482ea959a
leaderlord1 <gparaschiv@gmail.com>
2020-05-20 17:37:09 +0300

    fixed role change

--- php/admin/user_update.php
+++ php/admin/user_update.php
@@ -3,42 +3,41 @@
     require_once '../db_utils/database_conn.php';
     require_once '../userAccount/UserRoles.php';
 
+    session_start();
+    
     $action = null;
     $id = null;
-
-    if(isset($_POST['action']))
+   
+    if(isset($_POST['action']) && isset($_POST['id']) && isset($_SESSION['loggedin']) && $_SESSION['role'] == UserRoles::ADMIN){
         $action = $_POST['action'];
-    if(isset($_POST['id']))
         $id = $_POST['id'];
 
-    $sql = null;
-    $stmt = null;
+        $sql = null;
+        $stmt = null;
 
-    if($action == "ban") {
-        $sql = 'update users set role =' . UserRoles::BANNED . 'where id = :id';
-        $stmt = BD::obtine_conexiune()->prepare($sql);
-        $stmt -> execute ([
-        'id' => $id
-        ]);
+        if($action == "ban") {   
+            $sql = 'update users set role =' . UserRoles::BANNED . ' where id = :id';
+            $stmt = BD::obtine_conexiune()->prepare($sql);
+            if($stmt -> execute ([
+            'id' => $id
+            ])){echo 'Banned user ID=' . $id;};
+        }
+        else if($action == "unban") {
+            $sql = 'update users set role =' . UserRoles::USER . ' where id = :id';
+            $stmt = BD::obtine_conexiune()->prepare($sql);
+            if($stmt -> execute ([
+            'id' => $id
+            ])){echo 'Set normal user user ID=' . $id;};
+        } 
+        else if($action == "admin") {
+            $sql = 'update users set role =' . UserRoles::ADMIN . ' where id = :id';
+            $stmt = BD::obtine_conexiune()->prepare($sql);
+            if($stmt -> execute ([
+            'id' => $id
+            ])){echo 'Gave admin to user ID=' . $id;};
+        }
+
+        BD::opreste_conexiune();
+        exit();
     }
-
-    else if($action == "unban") {
-        $sql = 'update users set role =' . UserRoles::USER . 'where id = :id';
-        $stmt = BD::obtine_conexiune()->prepare($sql);
-        $stmt -> execute ([
-        'id' => $id
-        ]);
-    }
-
-    else if($action == "admin") {
-        $sql = 'update users set role =' . UserRoles::ADMIN . 'where id = :id';
-        $stmt = BD::obtine_conexiune()->prepare($sql);
-        $stmt -> execute ([
-        'id' => $id
-        ]);
-    }
-
-    $found = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-    echo json_encode($found); //this is sent as json to js/admin/user_ajaxsearch.js
-    exit();
 ?>
